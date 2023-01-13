10928142e7c2d4e84fa7d8257c6b7ead286817e7
Kayque Amado <kayque_sfa@outlook.com>
2023-01-13 16:26:55 -0300

    M

---
+++ constantes_1.dart
@@ -0,0 +1,10 @@
+
+import 'dart:io';
+
+void main() {
+  // Área da circunferencia = PI * raio * raio
+  
+  var texto = stdin.readLineSync();
+  print(texto);
+
+}
---
+++ variaveis_2.dart
@@ -0,0 +1,15 @@
+
+void main() {
+  var n1 = 2;
+  var n2 = 5;
+
+  //
+
+  var t1 = "A sua nota é: ";
+  print(t1 + (n1 * n2).toString()); // Converte a operação n1 * n2 em uma String
+
+  //
+
+  print(n1.runtimeType); // Mostra o tipo de variável .runtimeType
+
+} 
