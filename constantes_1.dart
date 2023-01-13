3038ddd7b4b5f2d6bd77d104df4d376a2ef27c1c
Kayque Amado <kayque_sfa@outlook.com>
2023-01-13 16:49:18 -0300

    área da circunferencia

--- constantes_1.dart
+++ constantes_1.dart
@@ -3,9 +3,13 @@ import 'dart:io';
 
 void main() {
   // Área da circunferencia = PI * raio * raio
+  final pi = 3.14;
 
+  stdout.write("Informe o raio: "); //Saída padrão (tela) isso deixa a saida e a entrada na mesma linha
   var entradadousuario = stdin.readLineSync();
   final raio = double.parse(entradadousuario!);     // .parse ==> String to Double
-  print("O valor do raio é: " + raio.toString());
+
+  var area = raio * pi * raio;
+  print("A área da circunferência é de: " + area.toString());
 
 }
